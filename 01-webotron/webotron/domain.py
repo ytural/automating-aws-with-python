1eec10c48000a5ea551d993ec0c3ff0a0170e6ef
Yuzbashiyev <tural@amazon.cz>
2019-10-25 00:37:59 +0200

    Configure DNS with Route53

---
+++ 01-webotron/webotron/domain.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""Classes for Route53 domains."""
+from uuid import uuid4
+
+
+
+class DomainManager:
+    """Manage an Route53 domains."""
+
+    def __init__(self, session):
+        """Create a DomainManager object."""
+        self.session = session
+        self.client = self.session.client('route53')
+
+    def find_hosted_zone(self, domain_name):
+        """Find zone matching domain_name."""
+        paginator = self.client.get_paginator('list_hosted_zones')
+        for page in paginator.paginate():
+            for zone in page['HostedZones']:
+                if domain_name.endswith(zone['Name'][:-1]):
+                    return zone
+        return None
+
+    def create_hosted_zone(self, domain_name):
+        """Create a hosted zone to matching domain_name."""
+        zone_name = '.'.join(domain_name.split('.')[-2:])+'.'
+        return self.client.create_hosted_zone(
+            Name=zone_name,
+            CallerReference=str(uuid4())
+        )
+
+    def create_s3_domain_record(self, zone, domain_name, endpoint):
+        """Create a domain record in zone for domain_name."""
+        return self.client.change_resource_record_sets(
+            HostedZoneId=zone['Id'],
+            ChangeBatch={
+                'Comment': 'Created by webotron.',
+                'Changes': [{
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': domain_name,
+                            'Type': 'A',
+                            'AliasTarget': {
+                                'HostedZoneId': endpoint.zone,
+                                'DNSName': endpoint.host,
+                                'EvaluateTargetHealth': False
+                                        }
+                        }
+                    }
+                ]
+            })
+
+
+
+
--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -11,28 +11,30 @@ Webotron automates the process of deploying static web sites
 - Configure DNS with AWS Route 53
 - Configure Content Delivery Network and SSL with AWS CloudFront
 """
-
+from pprint import pprint
 import boto3
 import click
 
 from bucket import BucketManager
-
+from domain import  DomainManager
+import util
 
 session = None
 bucket_manager = None
-
+domain_manager = None
 
 @click.group()
 @click.option('--profile', default=None, required=True,
               help="Use a given AWS profile.")
 def cli(profile):
     """Webotron deploys websites to AWS."""
-    global session, bucket_manager
+    global session, bucket_manager, domain_manager
     session_cfg = {}
     if profile:
         session_cfg['profile_name'] = profile
     session = boto3.Session(**session_cfg)
     bucket_manager = BucketManager(session)
+    domain_manager = DomainManager(session)
 
 
 @cli.command('list-buckets')
@@ -67,6 +69,17 @@ def sync(pathname, bucket):
     """Sync content of PATHNAME to BUCKET."""
     bucket_manager.sync(pathname, bucket)
     print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
+
+
+@cli.command('setup-domain')
+@click.argument('domain')
+def setup_domain(domain):
+    """Configure DOMAIN to point to BUCKET."""
+    bucket = domain
+    zone = domain_manager.find_hosted_zone(domain) or domain_manager.create_hosted_zone(domain)
+    endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
+    domain_manager.create_s3_domain_record(zone, domain, endpoint)
+    print("Domain configure: http://{}".format(domain))
 
 
 if __name__ == '__main__':
